b61d49fb3014e87639848894ffa57fba1e0a14d9
Aswin Raj D <aswinraj790417@gmail.com>
2026-02-10 21:48:40 +0530

    Add files via upload

---
+++ string part-1/problem-5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    scanf("%s",str);
+    int i=0;
+    int j=strlen(str)-1;
+    while(i<j){
+        if(str[i]!=str[j]){
+            printf("Not Palindrome");
+            return 0;
+        }
+        i++;
+        j--;
+    }
+    printf("Palindrome");
+    return 0;
+
+    
+    
+}
