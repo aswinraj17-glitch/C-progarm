cdf767c92bfde46567137f731dfc841c7597d929
Aswin Raj D <aswinraj790417@gmail.com>
2026-02-10 22:29:12 +0530

    Add files via upload

---
+++ string part-1/problem-12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    scanf("%s",str);
+    int len=strlen(str);
+    int count=0;
+    for(int i=0;i<len;i++){
+        if(str[i]=='@'|| str[i]=='#'||str[i]=='$'||str[i]=='%'||str[i]=='&' ){
+                count++;
+
+            }
+        }
+        printf("%d",count);
+}
