f21d890f0a23cf921aaf81cbb1b4a794a464b397
Aswin Raj D <aswinraj790417@gmail.com>
2026-02-10 21:50:56 +0530

    Add files via upload

---
+++ string part-1/problem-9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    scanf("%s",str);
+    int len=strlen(str);
+    for(int i=0;i<len;i++){
+        str[i]=toupper(str[i]);
+    }
+    printf("%s",str);
+    return 0; 
+
+}
