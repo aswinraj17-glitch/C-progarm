1715f4171bafad553215703ebd345fcb2a5eb0aa
Aswin Raj D <aswinraj790417@gmail.com>
2026-02-12 11:42:46 +0530

    Add files via upload

---
+++ Functions/problem-9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void palindrome(int n){
+    int rev=0;
+    int temp=n;
+    while(n!=0){
+        int digit=n%10;
+        rev=rev*10+digit;
+        n=n/10;
+    }
+    if(temp==rev){
+         printf("Palindrome");
+    }
+    else{
+        printf("Not Palindrome");
+    }
+    
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    palindrome(n);
+    return 0;
+}
