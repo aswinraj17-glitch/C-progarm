568b1414951ca7f95289a42d8876438be1f54f05
Aswin Raj D <aswinraj790417@gmail.com>
2026-02-12 09:37:16 +0530

    Add files via upload

---
+++ Functions/problem-7.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void prime(int n){
+    int count=0;
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            count++;
+        }
+        
+    }
+    if(count==2){
+        printf("Prime");
+    }
+    else{
+        printf("Not Prime");
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    prime(n);
+    return 0;
+}
