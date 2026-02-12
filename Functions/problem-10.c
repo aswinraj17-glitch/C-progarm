58cbdca6222e17579a8980a4966c6bebb1e58f7e
Aswin Raj D <aswinraj790417@gmail.com>
2026-02-12 12:00:42 +0530

    Add files via upload

---
+++ Functions/problem-10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void sum(int n){
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        sum=sum+i;
+    
+    }
+    printf("%d",sum);
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    sum(n);
+    return 0;
+}
