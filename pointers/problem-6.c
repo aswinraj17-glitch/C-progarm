8813a8abe3707b4960fc763b1e8a1c5e7c61c664
Aswin Raj D <aswinraj790417@gmail.com>
2026-02-14 13:15:52 +0530

    Add files via upload

---
+++ pointers/problem-6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void reverse(int *arr, int n) {
+    int *start=arr;
+    int *end=arr+n-1;
+    int temp;
+    while(start<end) {
+        temp=*start;
+        *start=*end;
+        *end=temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[100];
+    for (int i=0;i<n;i++) {
+        scanf("%d", &arr[i]);
+    }
+    reverse(arr,n);
+    for (int i=0;i<n;i++) {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
